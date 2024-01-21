8adfc50f5045ae89c2a22bf0b540cc566eeba87a
cyberorzech <natan.orze@gmail.com>
2024-01-21 09:02:30 +0100

    Upload vulnerable code that can be actually exploited (code execution, hardcoding secrets

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
+SECRET = "9ifewkjpmn0ij43t90j"
 
 def multiplicate(x: float, times: float, print_result: bool = False) -> float:
     if not print_result:
-        return x * times
-    print(eval('x * times'))
+        return float(x) * float(times)
+    print(eval('{}*{}'.format(x, times)))
     
 
 def main():
-    res = multiplicate(1.5, times=4)
-    print(f"Safe func run: {res}")
-    print("Unsafe func run: ", end="")
-    multiplicate(1.5, times=4, print_result=True)
+    """
+    To exploit, run python app.py and pass python variable or cmd as parameter, for example: __import__('os').system('date')
+    """
+    x = input("Pass first number:")
+    y = input("Pass second number:")
+    multiplicate(x, y, print_result=True)
 
 if __name__ == "__main__":
     main()
