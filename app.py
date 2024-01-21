db621b56f3153642dc97ce47ebd9b54f4c48f2bf
cyberorzech <natan.orze@gmail.com>
2024-01-21 07:39:52 +0100

    Implement multiplicate func with vulnerability

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-x = 1
-# insecure version
-# print(eval('x+1'))
 
-# secure version
-print("hello")
+def multiplicate(x: float, times: float, print_result: bool = False) -> float:
+    if not print_result:
+        return x * times
+    print(eval('x * times'))
+    
+
+def main():
+    res = multiplicate(1.5, times=4)
+    print(f"Safe func run: {res}")
+    print("Unsafe func run: ", end="")
+    multiplicate(1.5, times=4, print_result=True)
+
+if __name__ == "__main__":
+    main()
