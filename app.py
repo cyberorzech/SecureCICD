bd7d69decb15e155a650ba7430d3c81e269a4846
cyberorzech <50600293+cyberorzech@users.noreply.github.com>
2024-01-23 17:56:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ SECRET = "9ifewkjpmn0ij43t90j"
 
 def multiplicate(x: float, times: float, print_result: bool = False) -> float:
     if not print_result:
-        return float(x) * float(times)
+        return
+        # return float(x) * float(times)
     print(eval('{}*{}'.format(x, times)))
 
 
